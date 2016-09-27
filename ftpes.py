7a39233af140eb2bdacb1261ec7d839eb74e85fa
ludovitsiket <siket.ludovit@gmail.com>
2016-09-27 16:07:05 +0200

    oddelenie connect_ftpes() a fcie na stahovanie

--- ftpes.py
+++ ftpes.py
@@ -1,24 +1,26 @@
 from ftplib import FTP_TLS
 import os, sys
 
-def connect_ftpes(server,user,passwd):
-  ftps = FTP_TLS(server)
+def connect_ftpes(ftps,server,user,passwd):
   ftps.connect(server, 21)
   ftps.auth()
   ftps.prot_p()
   ftps.login(user, passwd)
   ftps.retrlines('LIST')
+  
+def download(ftps):
   filename=input("Meno súboru na stiahnutie: ")
   file = open(filename, 'wb')
   ftps.retrbinary('RETR %s' % filename, file.write)
   print ("Sťahovanie ukončené. Súbor sa nachádza v aktuálnom adresári.")
   ftps.close()
-  return
 
 def main():
   server=sys.argv[1]
   user=sys.argv[2]
   passwd=sys.argv[3]
-  connect_ftpes(server,user,passwd)
+  ftps = FTP_TLS(server)
+  connect_ftpes(ftps,server,user,passwd)
+  download(ftps)
 
 main()
