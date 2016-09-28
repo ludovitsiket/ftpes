c04dbaa60895fa2a0a0e32adc7c7791dfc34ffec
ludovitsiket <siket.ludovit@gmail.com>
2016-09-28 18:04:43 +0200

    prepnutie sa do urceneho adresara a stiahnutie vsetkych suborov

--- ftpes.py
+++ ftpes.py
@@ -6,21 +6,35 @@ def connect_ftpes(ftps,server,user,passwd):
   ftps.auth()
   ftps.prot_p()
   ftps.login(user, passwd)
+  ftps.cwd("Documents")
   ftps.retrlines('LIST')
   
-def download(ftps):
-  filename=input("Meno súboru na stiahnutie: ")
-  file = open(filename, 'wb')
-  ftps.retrbinary('RETR %s' % filename, file.write)
-  print ("Sťahovanie ukončené. Súbor sa nachádza v aktuálnom adresári.")
-  ftps.close()
+def download(ftps,filenames):
+  for item in filenames:
+    local_filename = os.path.join(r"C:\Documents and Settings\user\Plocha\ftpes", item)
+    print("Sťahujem: ",item, "do",local_filename)
+    file = open(local_filename, 'wb')
+    ftps.retrbinary('RETR '+ item, file.write)
+    file.close()
+    print ("Sťahovanie ukončené.")
+  ftps.quit()
+  
+def hint():
+  print("Nesprávny počet argumentov. Syntax: python ftpes.py <server> <prihlasovacie_meno> <heslo>")
+  
+def argument_control():
+  hint()
 
 def main():
-  server=sys.argv[1]
-  user=sys.argv[2]
-  passwd=sys.argv[3]
+  try:
+    server=sys.argv[1]
+    user=sys.argv[2]
+    passwd=sys.argv[3]
+  except:
+    argument_control()
   ftps = FTP_TLS(server)
   connect_ftpes(ftps,server,user,passwd)
-  download(ftps)
+  filenames=ftps.nlst()
+  download(ftps,filenames)
 
 main()
