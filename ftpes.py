fdfa1046d5d1709162476c864386279e2c41f856
ludovitsiket <siket.ludovit@gmail.com>
2016-09-29 13:49:27 +0200

    zlozka kam sa ma stahovat je zadavana ako posledny argument

--- ftpes.py
+++ ftpes.py
@@ -8,9 +8,10 @@ def connect_ftpes(ftps,server,user,passwd):
   ftps.login(user, passwd)
   ftps.retrlines('LIST')
   
-def download(ftps,filenames):
+def download(ftps,filenames,local_folder):
   for item in filenames:
-    local_filename = os.path.join(r"C:\Documents and Settings\user\Plocha\ftpes", item)
+    
+    local_filename = os.path.join(local_folder, item)
     print("Sťahujem: ",item, "do",local_filename)
     file = open(local_filename, 'wb')
     try:
@@ -20,20 +21,29 @@ def download(ftps,filenames):
     file.close()
     print ("Sťahovanie ukončené.")
   ftps.quit()
+
+def wrong_arguments():
+  print("Nesprávny počet argumentov. Syntax: python ftpes.py <server> <prihlasovacie_meno> <heslo> <lokalna_zlozka_pre_stahovanie (v uvodzovkach)>")
+  sys.exit()
   
 def argument_control():
-  print("Nesprávny počet argumentov. Syntax: python ftpes.py <server> <prihlasovacie_meno> <heslo>")
+  if len(sys.argv) < 5:
+    wrong_arguments() 
+  elif len(sys.argv) > 5:
+    wrong_arguments() 
 
 def main():
   try:
     server=sys.argv[1]
     user=sys.argv[2]
     passwd=sys.argv[3]
+    local_folder=sys.argv[4]
+    #local_folder=local_folder.decode('string_escape')
   except:
     argument_control()
   ftps = FTP_TLS(server)
   connect_ftpes(ftps,server,user,passwd)
   filenames=ftps.nlst()
-  download(ftps,filenames)
+  download(ftps,filenames,local_folder)
 
 main()
